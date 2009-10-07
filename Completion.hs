b0cd43e5655394652c0a38fd1604c088ee88a056
Matt Brubeck <mbrubeck@limpet.net>
2009-10-06 20:30:10 -0700

    Initial commit.

---
+++ Completion.hs
@@ -0,0 +1,9 @@
+import Data.List (isPrefixOf)
+
+type Completer = String -> [String]
+
+token :: String -> Completer
+token x y | y `isPrefixOf` x  = [x]
+          | otherwise         = []
+
+(<|>) :: Completer -> Completer -> Completer
