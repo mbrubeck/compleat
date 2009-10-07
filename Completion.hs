e1f56f9733ce8f1d9781f2284b2ede100802a0b6
Matt Brubeck <mbrubeck@limpet.net>
2009-10-07 07:46:10 -0700

    Copying concepts from Monadic Parser Combinators.

--- Completion.hs
+++ Completion.hs
@@ -1,14 +1,22 @@
 import Data.List (isPrefixOf)
 
-type Completer = [String] -> [([String], String)]
+type Completer = [String] -> [(String, [String])]
+
+zero :: Completer
+zero _ = []
+
+item :: Completer
+item []     = []
+item (x:xs) = [(x,xs)]
 
 token :: String -> Completer
-token t [x]    | x `isPrefixOf` t  = [([], t)]
-token t (x:xs) | x == t            = [(xs, t)]
+token t [x]    | x `isPrefixOf` t  = [(x, [])]
+token t (x:xs) | x == t            = [(x, xs)]
 token t _                          = []
 
 (<|>) :: Completer -> Completer -> Completer
 a <|> b = \x -> a x ++ b x
 
 (=>>) :: Completer -> Completer -> Completer
-a =>> b = concat . map (b . fst) . a
+a =>> b = concat . map (b . snd) . a
+
