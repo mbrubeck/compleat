3c01c2c2ef5144f9b3aac5baeee1c37e402c8f32
Matt Brubeck <mbrubeck@limpet.net>
2009-10-07 07:32:33 -0700

    Concatenation.

--- Completion.hs
+++ Completion.hs
@@ -1,10 +1,14 @@
 import Data.List (isPrefixOf)
 
-type Completer = String -> [String]
+type Completer = [String] -> [([String], String)]
 
 token :: String -> Completer
-token x y | y `isPrefixOf` x  = [x]
-          | otherwise         = []
+token t [x]    | x `isPrefixOf` t  = [([], t)]
+token t (x:xs) | x == t            = [(xs, t)]
+token t _                          = []
 
 (<|>) :: Completer -> Completer -> Completer
 a <|> b = \x -> a x ++ b x
+
+(=>>) :: Completer -> Completer -> Completer
+a =>> b = concat . map (b . fst) . a
