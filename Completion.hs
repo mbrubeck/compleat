f44160100634c28b866ca8ab02c29269647e8fe8
Matt Brubeck <mbrubeck@limpet.net>
2009-10-09 07:45:57 -0700

    Now it's a real Monadic Parser ala Hutton and Meijer.

--- Completion.hs
+++ Completion.hs
@@ -1,33 +1,25 @@
-import Data.List (isPrefixOf)
+import Control.Monad (MonadPlus, mzero, mplus)
+-- import Data.List (isPrefixOf)
 
-type Completer = [String] -> [(String, [String])]
+data Parser a = Parser { parse :: (String -> [(a, String)]) }
 
-(<|>) :: Completer -> Completer -> Completer
-a <|> b = \x -> a x ++ b x
+instance Monad Parser where
+    return a  = Parser (\cs -> [(a, cs)])
+    p >>= f   = Parser (\cs -> concat [parse (f a) cs' | (a, cs') <- parse p cs])
 
-bind :: Completer -> (String -> Completer) -> Completer
-a `bind` f = \inp -> concat [f st inp' | (st, inp') <- a inp]
+instance MonadPlus Parser where
+    mzero     = Parser (\cs -> [])
+    mplus p q = Parser (\cs -> parse p cs ++ parse q cs)
 
-(=>>) :: Completer -> Completer -> Completer
-a =>> b = a `bind` \_ -> b
+(<|>) :: Parser a -> Parser a -> Parser a
+(<|>) = mplus
 
-zero :: Completer
-zero _ = []
+item :: Parser Char
+item = Parser (\cs -> case cs of
+                        ""     -> []
+                        (c:cs) -> [(c, cs)])
 
-result :: String -> Completer
-result x inp = [(x,inp)]
-
-item :: Completer
-item []     = []
-item (x:xs) = [(x,xs)]
-
-sat :: (String -> Bool) -> Completer
-sat p = item `bind` \x ->
-        if p x then result x else zero
-
-token :: String -> Completer
-token t [x]    | x `isPrefixOf` t  = [(t, [])]
-token t (x:xs) | x == t            = [(x, xs)]
-token t _                          = []
-
-
+sat :: (Char -> Bool) -> Parser Char
+sat p = do 
+    x <- item
+    if p x then return x else mzero
