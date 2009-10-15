225cb69cdb7f35b49fb0222d8e5ccbe2b9aac2f8
Matt Brubeck <mbrubeck@limpet.net>
2009-10-15 08:33:27 -0700

    Fix brackets.

--- Usage.hs
+++ Usage.hs
@@ -5,50 +5,48 @@ import Text.Parsec.Prim ((<|>), many, try, runParser)
 import Text.Parsec.String (Parser)
 import qualified Completer as C
 import qualified Text.Parsec.Token as T
-import Text.Parsec.Language (haskellStyle)
+import Text.Parsec.Language (javaStyle)
 
 terms :: Parser C.Completer
 terms = do
     cs <- many term
     return (foldl (C.-->) C.continue cs)
 
-term :: Parser C.Completer
-term = repeated (group <|> str) C.many1 id
+term = repeated (choiceGroup <|> str) C.many1 id
    <|> repeated optionGroup C.many C.optional
 
-choice :: Parser C.Completer
-choice = chainl1 term (symbol "|" >> return (C.<|>))
+optionGroup = do
+    c <- brackets (try choice <|> terms)
+    return c
 
-group :: Parser C.Completer
-group = parens item
+choiceGroup = parens choice
 
-optionGroup :: Parser C.Completer
-optionGroup = brackets item
+choice = do
+    c <- term
+    symbol "|"
+    d <- (try choice <|> term)
+    return (c C.<|> d)
+
+str = do
+    s <- stringLiteral <|> identifier
+    return (C.str s)
 
 repeated :: Parser a -> (a -> b) -> (a -> b) -> Parser b
 repeated p f g = do
     c <- p
     try (symbol "..." >> return (f c)) <|> return (g c)
 
-item :: Parser C.Completer
-item = try terms <|> choice
-
-str :: Parser C.Completer
-str = do
-    s <- stringLiteral <|> identifier
-    return (C.str s)
-
--- Lexer?
+-- Lexer
 
 lexer :: T.TokenParser ()
-lexer  = T.makeTokenParser haskellStyle
+lexer  = T.makeTokenParser javaStyle
 
 whiteSpace    = T.whiteSpace lexer
 lexeme        = T.lexeme lexer
 symbol        = T.symbol lexer
 natural       = T.natural lexer
 parens        = T.parens lexer
-brackets      = T.parens lexer
+brackets      = T.brackets lexer
 semi          = T.semi lexer
 identifier    = T.identifier lexer
 reserved      = T.reserved lexer
