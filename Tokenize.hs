c1f83ae87dcb54314f1c0b2773ac659478b5c8a3
Matt Brubeck <mbrubeck@limpet.net>
2009-10-15 19:06:18 -0700

    Experimenting with the Parsec tokenizer.

--- Tokenize.hs
+++ Tokenize.hs
@@ -1,6 +1,6 @@
 module Tokenize () where
 
-import Text.Parsec.Char (anyChar, char, noneOf, space)
+import Text.Parsec.Char (anyChar, char, noneOf, space, spaces)
 import Text.Parsec.Combinator (between, eof, many1, manyTill)
 import Text.Parsec.Prim ((<|>), many, try, runParser)
 import Text.Parsec.String (Parser)
@@ -8,16 +8,18 @@ import Text.Parsec.String (Parser)
 tokens :: Parser [String]
 tokens = many token
 
-token = try (many space >> (quoted '"' <|> quoted '\'' <|> unquoted))
-    <|> (many1 space >> eof >> return "")
-
-unquoted :: Parser String
-unquoted = manyTill (escaped <|> anyChar) (try space)
+token = spaces >> (quoted '"' <|> quoted '\'' <|> unquoted)
 
 quoted :: Char -> Parser String
 quoted q = do
     char q
-    manyTill (escaped <|> anyChar) (try (char q) <|> (eof >> return '_'))
+    manyTill (escaped <|> anyChar) (try (char q >> return ()) <|> eof)
+
+unquoted :: Parser String
+unquoted = do
+    c <- anyChar
+    cs <- manyTill (escaped <|> anyChar) (try (space >> return ()) <|> eof)
+    return (c:cs)
 
 escaped :: Parser Char
 escaped = do
