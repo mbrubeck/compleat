7665d32a026759c42ff81d706272b8941fe572b2
Matt Brubeck <mbrubeck@limpet.net>
2009-10-25 16:57:39 -0700

    Build an environment from the usage file.

--- Usage.hs
+++ Usage.hs
@@ -3,6 +3,7 @@ module Usage (fromFile) where
 import qualified Completer as C
 import Text.Parsec
 import Text.Parsec.Language (javaStyle)
+import Text.Parsec.String (Parser, parseFromFile)
 import qualified Text.Parsec.Token as T
 
 data Usage = Primitive C.Completer | Var String
@@ -11,35 +12,47 @@ data Usage = Primitive C.Completer | Var String
 
 fromFile :: String -> IO C.Completer
 fromFile fileName = do
-    input  <- readFile fileName
-    let result = runParser usage [] fileName input
+    result <- parseFromFile usage fileName
     case result of
-        Right u  -> return (eval [] u)
-        Left err -> error (show err)
+        Right env -> return (eval env $ mainCommand env)
+        Left err  -> error (show err)
 
 -- Evaluator
 
 type Environment = [(String,Usage)]
 
-eval :: [(String,Usage)] -> Usage -> C.Completer
+eval :: Environment -> Usage -> C.Completer
 eval env (Primitive c) = c
-eval env (Var s)       = if s == "file" then C.file else C.skip
 eval env (Choice xs)   = foldl1 (C.<|>) (map (eval env) xs)
 eval env (Sequence xs) = foldl1 (C.-->) (map (eval env) xs)
 eval env (Many x)      = C.many     (eval env x)
 eval env (Many1 x)     = C.many1    (eval env x)
 eval env (Optional x)  = C.optional (eval env x)
+eval env (Var s)       = if s == "file" then C.file else C.skip
 
--- Parser
+mainCommand :: Environment -> Usage
+mainCommand env = Choice $ map snd $ filter ((""==) . fst) env
 
-type Parser = Parsec String Environment
+-- Top-level parser
 
-usage :: Parser Usage
-usage = do
-    whiteSpace
-    xs <- sepEndBy1 command (symbol ";")
-    return (Choice xs)
+usage :: Parser [(String, Usage)]
+usage = whiteSpace >> sepEndBy1 (try definition <|> mainDefinition) (symbol ";")
 
+definition :: Parser (String, Usage)
+definition = do
+    s <- atom
+    symbol "="
+    u <- pattern
+    return (s, u)
+
+mainDefinition :: Parser (String, Usage)
+mainDefinition = do
+    u <- command
+    return ("", u)
+
+-- Usage parser
+
+command :: Parser Usage
 command = do
     x <- commandName 
     y <- pattern
@@ -65,20 +78,20 @@ str = do
     s <- atom
     return $ Primitive (C.str s)
 
-repeated :: Parser a -> (a -> b) -> (a -> b) -> Parser b
-repeated p f g = p >>= \x ->
-    try (symbol "..." >> return (f x)) <|> return (g x)
-
 variable = do
     s <- between (symbol "<") (symbol ">") atom
     return (Var s)
+
+repeated :: Parser a -> (a -> b) -> (a -> b) -> Parser b
+repeated p f g = p >>= \x ->
+    try (symbol "..." >> return (f x)) <|> return (g x)
 
 atom :: Parser String
 atom = stringLiteral <|> lexeme (many1 (alphaNum <|> oneOf "-_/@=+.,:"))
 
 -- Lexer
 
-lexer :: T.TokenParser Environment
+lexer :: T.TokenParser ()
 lexer  = T.makeTokenParser javaStyle
 
 lexeme        = T.lexeme lexer
